c6c82ff838905a37e1e72b271fa02aa405c5fb7d
sarthakk1890 <144singhsarthak@gmail.com>
2024-04-04 22:45:30 +0530

    Added button

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,21 @@
+import { useNavigate } from 'react-router-dom'
 import '../styles/home.scss'
 
+
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/products/64da35c9f2805549788a4fc6')
+    }
+
     return (
         <div className='center home-main'>
             <h1 className="gradient-text center">Welcome to BharatPOS</h1>
+            <button onClick={handleClick}>
+                Click Me
+            </button>
         </div>
     )
 }
