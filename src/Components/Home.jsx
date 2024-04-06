ffa0dc28cbe47015a0d5b516563e0fb74e1d0793
Sarthak Singh <144singhsarthak@gmail.com>
2024-04-06 22:43:31 +0530

    Changed Base_URL

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,18 +4,9 @@ import '../styles/home.scss'
 
 const Home = () => {
 
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate('/products/1111121111')
-    }
-
     return (
         <div className='center home-main'>
             <h1 className="gradient-text center">Welcome to BharatPOS</h1>
-            <button onClick={handleClick}>
-                Click Me
-            </button>
         </div>
     )
 }
